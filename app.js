26b1c030fd421ed7a2ae475753e8f25fa0c17f8d
Bruno <brunomuhlmann.indrema@gmail.com>
2025-03-05 23:34:34 +0000

    Version updated to 1.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Versão 4.0');
+  res.send('Hello World! Versão 1.0');
 });
 
 app.listen(port, () => {
